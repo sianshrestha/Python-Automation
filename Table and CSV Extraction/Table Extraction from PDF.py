0a2ce62bc444df859537196df06f47ed6ef42652
Siddhanta Shrestha <140154267+sianshrestha@users.noreply.github.com>
2024-12-29 03:06:28 +0100

    Add files via upload

---
+++ Table and CSV Extraction/Table Extraction from PDF.py
@@ -0,0 +1,7 @@
+import camelot
+
+tables = camelot.read_pdf('foo.pdf', pages='1', flavor='lattice')
+
+tables.export('foo.csv', f='csv', compress=True)
+tables[0].to_csv('foo.csv')  # to a csv file
+print(tables[0].df)
